e29c1dedd5c862964b09e5fdc5211fafef2a41d6
Веталь Кросс <vetaly.kross@gmail.com>
2025-05-18 17:59:29 +0300

    Оновив hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,30 @@
+fn invert_the_case(s: String) -> String {
+    let mut result = String::new();
+    for c in s.chars() {
+        if c.is_lowercase() {
+            result.push(c.to_uppercase().next().unwrap());
+        } else if c.is_uppercase() {
+            result.push(c.to_lowercase().next().unwrap());
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            ("Hello", "hELLO"),
+            ("Привет", "пРИВЕТ"),
+        ];
+        for (a, b) in data.iter() {
+            assert_eq!(invert_the_case(a.to_string()), b.to_string());
+            assert_eq!(invert_the_case(b.to_string()), a.to_string());
+        }
+    }
+}
