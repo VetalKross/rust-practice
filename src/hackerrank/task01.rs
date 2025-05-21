587f304e6c340041f172fe8b725b4b66c859ea01
Веталь Кросс <vetaly.kross@gmail.com>
2025-05-21 22:16:36 +0300

    Додав першузадачу simple_array_sum для hackerrank

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,25 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+use std::io;
+
+fn simple_array_sum(ar: &[i32]) -> i32 {
+    let mut sum = 0;
+    for i in 0..ar.len() {
+        sum += ar[i];
+    }
+    sum
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap(); 
+
+    let numbers: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let result = simple_array_sum(&numbers);
+    println!("{}", result);
 }
